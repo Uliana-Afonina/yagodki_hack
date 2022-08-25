f268cded348fb24afd9c12d37c232021a0acfd94
Uliana Afonina <afonina.uliana@otr.ru>
2022-08-25 16:33:21 +0300

    Initial commit

---
+++ src/main/java/com/afoninauliana/springboot/yagodki_hack/YagodkiHackApplication.java
@@ -0,0 +1,13 @@
+package com.afoninauliana.springboot.yagodki_hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagodkiHackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YagodkiHackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afoninauliana/springboot/yagodki_hack/YagodkiHackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afoninauliana.springboot.yagodki_hack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagodkiHackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
